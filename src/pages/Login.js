8649b1651dbe1d2f9832641d97c5d45928fe9232
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-07-21 15:55:24 -0300

    requisito 2 com ressalvas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,68 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-login">Login</div>
-    );
+export default class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isButtonDisabled: true,
+      isLoading: false,
+      login: false,
+      nome: '',
+    };
   }
-}
 
-export default Login;
+      caracteres = (event) => {
+        const CAR_MIN = 3;
+        const { value } = event.target;
+        if (value.length <= CAR_MIN) this.setState({ isButtonDisabled: true });
+        if (value.length >= CAR_MIN) this.setState({ isButtonDisabled: false });
+        this.setState({ nome: value });
+      };
+
+      cCreateUser = () => {
+        const TIME_CARG = 1000;
+        const { nome } = this.state;
+        createUser({ name: nome });
+        this.setState({ isLoading: true });
+        setTimeout(() => this.setState({
+          isLoading: false,
+          login: true,
+        }), TIME_CARG);
+      };
+
+      render() {
+        const {
+          isButtonDisabled,
+          isLoading,
+          login,
+          value,
+        } = this.state;
+        return (
+          <div data-testid="page-login">
+            { login && <Redirect to="/search" /> }
+            { isLoading ? <Loading />
+              : (
+                <form onSubmit={ value }>
+                  <input
+                    data-testid="login-name-input"
+                    type="text"
+                    value={ value }
+                    onChange={ this.caracteres }
+                  />
+                  <button
+                    data-testid="login-submit-button"
+                    type="button"
+                    disabled={ isButtonDisabled }
+                    onClick={ this.cCreateUser }
+                  >
+                    Entrar
+                  </button>
+                </form>) }
+          </div>
+        );
+      }
+}
