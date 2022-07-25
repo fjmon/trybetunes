cda6d3c406b2d32861c7a394d918a489f2d9aba4
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-07-24 21:27:43 -0300

    requisito 5 com modificação

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isButtonDisabled: true,
+    };
+  }
+
+  caracteres = (event) => {
+    const { value } = event.target;
+    if (value.length >= 2) this.setState({ isButtonDisabled: false });
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <h1>Search</h1>
         <Header />
+        <form className="search-form">
+          <label htmlFor="search-artist-input">
+            <input
+              name="artist"
+              data-testid="search-artist-input"
+              onChange={ this.caracteres }
+              id="artist"
+              type="text"
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ isButtonDisabled }
+              type="button"
+              onClick={ this.pesquisar }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
