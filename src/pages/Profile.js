d58cd1e8ed2dd6c3f28ab2bdf1bfcb0d23ab1c9c
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-07-26 17:39:22 -0300

    requisito 13 sera que enfim terminado

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,63 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 import Header from './Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
-class Profile extends React.Component {
+export default class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.cargUser();
+  }
+
+  cargUser = async () => {
+    this.setState({
+      isLoading: true,
+    });
+    const getUseres = await getUser();
+    this.setState({
+      user: getUseres,
+      isLoading: false,
+    });
+  }
+
   render() {
+    const { isLoading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <h1>Profile</h1>
         <Header />
+        { isLoading ? <Loading />
+          : (
+            <>
+              <img
+                data-testid="profile-image"
+                src={ user.image }
+                alt={ user.name }
+              />
+              <div>{ user.name }</div>
+              <div>{ user.email }</div>
+              <div>{ user.description }</div>
+              <Route>
+                <Link
+                  data-testid="link-to-profileedit"
+                  to="/profile/edit"
+                >
+                  Editar perfil
+                </Link>
+              </Route>
+
+            </>
+
+          ) }
       </div>
     );
   }
 }
-
-export default Profile;
